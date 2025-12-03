a2d474fe639042e632a4af1d9d8eae7675e4007e
Majus-Dev <83781075+Majus-Dev@users.noreply.github.com>
2025-12-03 22:02:10 +0100

    eh

--- day3/day3part2.mjs
+++ day3/day3part2.mjs
@@ -6,17 +6,10 @@ readFile("./day3/input.txt", (err, data) => {
   let list = data.toString().split("\n");
   let total = 0;
 
+  const count = 12;
+
   list.forEach((b) => {
     let len = b.length;
-    let high = 0;
-
-    for (let i = 0; i < len - 1; i++) {
-      for (let j = i + 1; j < len; j++) {
-        let num = Number(`${b[i]}${b[j]}`);
-        if (num > high) high = num;
-      }
-    }
-    total += high;
   });
 
   console.log(total);
