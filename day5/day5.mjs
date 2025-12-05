274c6022ee549ca7477672aa99ecd0c0a6374d09
Majus-Dev <83781075+Majus-Dev@users.noreply.github.com>
2025-12-05 08:26:20 +0100

    day 5

---
+++ day5/day5.mjs
@@ -0,0 +1,31 @@
+import { readFile } from "fs";
+
+readFile("./day5/input.txt", (err, data) => {
+  if (err) throw err;
+
+  let total = 0;
+
+  const ranges = data
+    .toString()
+    .replaceAll("\r", "")
+    .split("\n")
+    .filter((a) => a.includes("-"));
+  const ingredients = data
+    .toString()
+    .replaceAll("\r", "")
+    .split("\n")
+    .filter((a) => !a.includes("-") && a != "");
+
+  for (let ingredient of ingredients) {
+    let unspoiled = false;
+    for (let range of ranges) {
+      let low = Number(range.split("-")[0]);
+      let high = Number(range.split("-")[1]);
+      if (Number(ingredient) >= low && Number(ingredient) <= high)
+        unspoiled = true;
+    }
+    if (unspoiled) total++;
+  }
+
+  console.log(ranges, ingredients, total);
+});
